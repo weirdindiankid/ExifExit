03635e8efc2ad7204a7efeb0c2c5dac41db91e6c
Dharmesh Tarapore <dharmesh@tarapore.ca>
2025-11-23 14:14:51 -0800

    Updated license header.

--- ExifExit MessagesExtension/MessagesViewController.swift
+++ ExifExit MessagesExtension/MessagesViewController.swift
@@ -4,6 +4,22 @@
 //
 //  Created by Dharmesh Tarapore on 11/23/25.
 //
+//  ExifExit - iMessage extension for stripping photo/video metadata
+//  Copyright (C) 2025 Dharmesh Tarapore <dharmesh@tarapore.ca>
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Affero General Public License as published
+//  by the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU Affero General Public License for more details.
+//
+//  You should have received a copy of the GNU Affero General Public License
+//  along with this program. If not, see <https://www.gnu.org/licenses/>.
+//
 
 import UIKit
 import Messages
